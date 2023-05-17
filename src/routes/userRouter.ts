f129e88378784c88e4d89ca81f0d005bf9a3ecf8
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-05-17 18:42:44 -0300

    adding get request to users and  got working rent posting

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { CreateUserController } from "../modules/users/useCases/createUser/CreateUserController";
+import { GetUserController } from "../modules/users/useCases/getUser/GetUserController";
 
 const createUserController = new CreateUserController();
+const getUserController = new GetUserController();
 
 const userRoutes = Router();
 
 userRoutes.post("/", createUserController.handle);
+userRoutes.get("/allUsers", getUserController.handle);
 
 export {userRoutes};
